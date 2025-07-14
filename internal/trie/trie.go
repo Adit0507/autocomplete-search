d01c735d24c3d330a46c405165f99201e00b178f
Adit0507 <adi05072002@gmail.com>
2025-07-14 12:56:52 +0530

    collectSuggestions recursively gather words with a given prefix. sortSuggestions ranks suggestions by frequency in descending order

--- internal/trie/trie.go
+++ internal/trie/trie.go
@@ -1,5 +1,7 @@
 package trie
 
+import "github.com/Adit0507/autocomplete-search/internal/models"
+
 type Node struct {
 	Children  map[rune]*Node
 	IsEnd     bool
@@ -26,4 +28,52 @@ func (t *Trie) Insert(word string, freq int) {
 
 	node.IsEnd = true
 	node.Frequency = freq
+}
+
+func (t *Trie) Search(prefix string, limit int) []models.Suggestion {
+	node := t.Root
+
+	for _, char := range prefix {
+		if _, exists := node.Children[char]; !exists {
+			return nil
+		}
+
+		node = node.Children[char]
+	}
+
+	return t.collectSuggestions(node, prefix, limit)
+}
+
+func (t *Trie) collectSuggestions(node *Node, prefix string, limit int) []models.Suggestion {
+	suggestions := []models.Suggestion{}
+	if node.IsEnd {
+		suggestions = append(suggestions, models.Suggestion{Text: prefix, Frequency: node.Frequency})
+	}
+
+	for char, child := range node.Children {
+		childSuggestions := t.collectSuggestions(child, prefix+string(char), limit)
+		suggestions = append(suggestions, childSuggestions...)
+
+		if len(suggestions) >= limit {
+			break
+		}
+	}
+
+	return sortSuggestions(suggestions, limit)
+}
+
+func sortSuggestions(suggestions []models.Suggestion, limit int) []models.Suggestion {
+	for i := 0; i < len(suggestions)-1; i++ {
+		for j := i + 1; j < len(suggestions); j++ {
+			if suggestions[i].Frequency < suggestions[j].Frequency {
+				suggestions[i], suggestions[j] = suggestions[j], suggestions[i]
+			}
+		}
+	}
+
+	if len(suggestions) > limit {
+		return suggestions[:limit]
+	}
+
+	return suggestions
 }
